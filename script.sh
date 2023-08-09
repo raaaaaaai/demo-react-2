ff4963cf7f157bb1d40b453f021c3f70ad45b99c
Rai Muhammad Awais <96009190+raaaaaaai@users.noreply.github.com>
2023-08-09 12:49:41 +0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+
+
+docker build -t reactapp:1.0 .
+docker tag reactapp:1.0 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
+
+# Push the Docker image to ECR
+docker push 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
