2721d34dbb6c67e361bd7061ce4501b94b9019fb
Rai Muhammad Awais <96009190+raaaaaaai@users.noreply.github.com>
2023-08-09 11:29:22 +0500

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,2 +1,11 @@
-cd /var/www/html
-serve -s build
+#!/bin/bash
+
+# Login to ECR
+$(aws ecr get-login --no-include-email --region ap-south-1)
+
+# Build and tag the Docker image
+docker build -t reactapp:1.0 .
+docker tag reactapp:1.0 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
+
+# Push the Docker image to ECR
+docker push 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
