584c0295f608dfbc6f1e8eef3a575c183950ba1f
Rai Muhammad Awais <96009190+raaaaaaai@users.noreply.github.com>
2023-08-10 10:20:04 +0500

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Login to ECR
-$(aws ecr get-login --no-include-email --region ap-south-1)
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 154020000542.dkr.ecr.ap-south-1.amazonaws.com
+# pull docker image
+docker pull 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
 
-# Build and tag the Docker image
-docker build -t reactapp:1.0 .
-docker tag reactapp:1.0 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
-
-# Push the Docker image to ECR
-docker push 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
+# run the docker image
+docker run -d -p 3000:3000 154020000542.dkr.ecr.ap-south-1.amazonaws.com/react-app:latest
